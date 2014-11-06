b646dd987468bdd9c24180f1907c070e7a5acb4e
Tomasz Polanski <tomasz.polanski@futurice.com>
2014-11-06 09:27:05 +0100

    Improved select switch example

--- Sandbox.UILogic/ViewModels/SelectSwitchPageViewModel.cs
+++ Sandbox.UILogic/ViewModels/SelectSwitchPageViewModel.cs
@@ -16,30 +16,34 @@ namespace Sandbox.UILogic.ViewModels
 
         public SelectSwitchPageViewModel()
         {
-            CitiesList = Enumerable.Repeat(0, 5)
-                                   .Select(number => new City { Population = (uint)number})
+            CitiesList = Enumerable.Range(0, 5)
+                                   .Select(number => new City { Population = (uint)number * 1000})
                                    .ToList();
 
             ItemSelectedCommand = new ReactiveCommand<IList<object>>();
-            Population = ItemSelectedCommand.Select(selectedItems => selectedItems.FirstOrDefault())
-                                            .OfType<City>()
-                                            .Select(city => Observable.FromEventPattern<long>(h => city.PopulationChanged += h,
-                                                                                              h => city.PopulationChanged -= h)
-                                                                      .Select(args => args.EventArgs)
-                                                                      .StartWith(city.Population))
+            Population = ItemSelectedCommand.Select(selectedItems => selectedItems.OfType<City>()
+                                                                                  .FirstOrDefault())
+                                            .Select(city => city != null ? Observable.FromEventPattern<long>(h => city.PopulationChanged += h,
+                                                                                                             h => city.PopulationChanged -= h)
+                                                                                     .Select(args => args.EventArgs)
+                                                                                     .StartWith(city.Population)
+                                                                         : Observable.Return<long>(0))
                                             .Switch()
+                                            .StartWith(0)
                                             .Select(population => population.ToString())
                                             .ToReadonlyReactiveProperty();
 
             Population = ItemSelectedCommand.Select(selectedItems => selectedItems.OfType<City>()
                                                                                   .ToList())
-                                            .Select(selectedItems => selectedItems.ToObservable()
-                                                                                  .SelectMany(city => Observable.FromEventPattern<long>(h => city.PopulationChanged += h,
-                                                                                                                                        h => city.PopulationChanged -= h)
-                                                                                                                .Select(args => city)
-                                                                                                                .StartWith(city))
-                                                                                  .BucketSum(city => city.Population))
+                                            .Select(selectedItems => selectedItems.Any() ? selectedItems.ToObservable()
+                                                                                                        .SelectMany(city => Observable.FromEventPattern<long>(h => city.PopulationChanged += h,
+                                                                                                                                                              h => city.PopulationChanged -= h)
+                                                                                                                                      .Select(args => city)
+                                                                                                                                      .StartWith(city))
+                                                                                                        .BucketSum(city => city.Population)
+                                                                                         : Observable.Return<long>(0))
                                             .Switch()
+                                            .StartWith(0)
                                             .Select(population => population.ToString())
                                             .ToReadonlyReactiveProperty();
 
